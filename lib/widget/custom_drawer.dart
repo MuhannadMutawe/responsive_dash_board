e21301b3e034626bc8f26abbee9c1af9eecf2368
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-02-14 13:10:39 +0200

    create drawer item list view.

--- lib/widget/custom_drawer.dart
+++ lib/widget/custom_drawer.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widget/drawer_item_list_view.dart';
 import 'package:responsive_dash_board/widget/user_info_list_tile.dart';
 
 class CustomDrawer extends StatelessWidget {
@@ -16,6 +17,10 @@ class CustomDrawer extends StatelessWidget {
             title: 'Lekan Okeowo',
             subtitle: 'demo@gmail.com',
           ),
+          SizedBox(
+            height: 8,
+          ),
+          DrawerItemListView(),
         ],
       ),
     );
---
+++ lib/widget/drawer_item.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:responsive_dash_board/models/drawer_item_model.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+
+class DrawerItem extends StatelessWidget {
+  const DrawerItem({super.key, required this.drawerItem});
+
+  final DrawerItemModel drawerItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: SvgPicture.asset(drawerItem.image),
+      title: Text(
+        drawerItem.title,
+        style: AppStyles.styleMedium16,
+      ),
+    );
+  }
+}

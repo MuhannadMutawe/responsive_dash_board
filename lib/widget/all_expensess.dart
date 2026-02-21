17180e79bfd103b4f835efefcd6513da66515b83
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-02-21 16:22:34 +0200

    all expensess item list view done.!!

--- lib/widget/all_expensess.dart
+++ lib/widget/all_expensess.dart
@@ -1,8 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:responsive_dash_board/models/all_expensess_item_model.dart';
-import 'package:responsive_dash_board/utils/app_images.dart';
 import 'package:responsive_dash_board/widget/all_expensess_header.dart';
-import 'package:responsive_dash_board/widget/all_expensess_item.dart';
+import 'package:responsive_dash_board/widget/all_expensess_items_list_view.dart';
 
 class AllExpensess extends StatelessWidget {
   const AllExpensess({super.key});
@@ -23,14 +21,7 @@ class AllExpensess extends StatelessWidget {
           SizedBox(
             height: 16,
           ),
-          AllExpensessItem(
-            itemModel: AllExpensessItemModel(
-              image: Assets.imagesIncome,
-              title: 'title',
-              date: '15/8/2008',
-              price: r'$1535.5',
-            ),
-          ),
+          AllExpensessItemsListView(),
         ],
       ),
     );
---
+++ lib/widget/all_expensess_items_list_view.dart
@@ -0,0 +1,47 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/models/all_expensess_item_model.dart';
+import 'package:responsive_dash_board/utils/app_images.dart';
+import 'package:responsive_dash_board/widget/all_expensess_item.dart';
+
+class AllExpensessItemsListView extends StatelessWidget {
+  const AllExpensessItemsListView({super.key});
+
+  static const items = [
+    AllExpensessItemModel(
+      image: Assets.imagesBalance,
+      title: 'Balance',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+    AllExpensessItemModel(
+      image: Assets.imagesIncome,
+      title: 'Income',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+    AllExpensessItemModel(
+      image: Assets.imagesExpenses,
+      title: 'Expenses',
+      date: 'April 2022',
+      price: r'$20,129',
+    ),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: items.asMap().entries.map(
+        (e) {
+          int index = e.key;
+          var item = e.value;
+          return Expanded(
+            child: Padding(
+              padding: EdgeInsets.symmetric(horizontal: index == 1 ? 12 : 0),
+              child: AllExpensessItem(itemModel: item),
+            ),
+          );
+        },
+      ).toList(),
+    );
+  }
+}

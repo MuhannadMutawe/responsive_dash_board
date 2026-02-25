b4f1e7c205d0b0512a1e0fa77b169306b8a76be0
Muhannad R Mutawe <muhannadrm208@gmail.com>
2026-02-25 21:35:31 +0200

    create latest transction & latest transction list view widgest.

---
+++ lib/widget/latest_transction.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:responsive_dash_board/utils/app_styles.dart';
+import 'package:responsive_dash_board/widget/latest_transction_list_view.dart';
+
+class LatestTransction extends StatelessWidget {
+  const LatestTransction({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Text(
+          'Latest Transction',
+          style: AppStyles.styleMedium16,
+        ),
+        SizedBox(
+          height: 16,
+        ),
+        LatestTransctionListView(),
+      ],
+    );
+  }
+}

22cf2e3b00fc05636d2d1233b5848fa6fe000b6e
MuhannadMutawe <muhannadrm208@gmail.com>
2026-03-15 15:57:13 +0200

    finished income char.

--- lib/widget/income_char.dart
+++ lib/widget/income_char.dart
@@ -1,33 +1,59 @@
 import 'package:fl_chart/fl_chart.dart';
 import 'package:flutter/material.dart';
 
-class IncomeChar extends StatelessWidget {
+class IncomeChar extends StatefulWidget {
   const IncomeChar({super.key});
 
   @override
+  State<IncomeChar> createState() => _IncomeCharState();
+}
+
+class _IncomeCharState extends State<IncomeChar> {
+  int activeIndex = -1;
+
+  @override
   Widget build(BuildContext context) {
-    return PieChart(getCharData());
+    return AspectRatio(
+      aspectRatio: 1,
+      child: PieChart(getCharData()),
+    );
   }
 
   PieChartData getCharData() {
     return PieChartData(
+      pieTouchData: PieTouchData(
+        enabled: true,
+        touchCallback: (p0, pietouchResponse) {
+          activeIndex =
+              pietouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+          setState(() {});
+        },
+      ),
       sectionsSpace: 0,
       sections: [
         PieChartSectionData(
+          showTitle: false,
           value: 40,
-          color: Color(0xff208CC8),
+          radius: activeIndex == 0 ? 35 : 25,
+          color: const Color(0xFF208BC7),
         ),
         PieChartSectionData(
           value: 25,
-          color: Color(0xff4EB7F2),
+          radius: activeIndex == 1 ? 35 : 25,
+          showTitle: false,
+          color: const Color(0xFF4DB7F2),
         ),
         PieChartSectionData(
           value: 20,
-          color: Color(0xff064061),
+          radius: activeIndex == 2 ? 35 : 25,
+          showTitle: false,
+          color: const Color(0xFF064060),
         ),
         PieChartSectionData(
           value: 22,
-          color: Color(0xffE2DECD),
+          radius: activeIndex == 3 ? 35 : 25,
+          showTitle: false,
+          color: const Color(0xFFE2DECD),
         ),
       ],
     );

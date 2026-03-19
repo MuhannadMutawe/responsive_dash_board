146e0e810224db92a1524c8ac15b07c9e177894e
MuhannadMutawe <muhannadrm208@gmail.com>
2026-03-19 22:12:46 +0200

    create detailed income chart.

---
+++ lib/widget/detailed_income_chart.dart
@@ -0,0 +1,61 @@
+import 'package:fl_chart/fl_chart.dart';
+import 'package:flutter/material.dart';
+
+class DetailedIncomeChart extends StatefulWidget {
+  const DetailedIncomeChart({super.key});
+
+  @override
+  State<DetailedIncomeChart> createState() => _DetailedIncomeChartState();
+}
+
+class _DetailedIncomeChartState extends State<DetailedIncomeChart> {
+  int activeIndex = -1;
+
+  @override
+  Widget build(BuildContext context) {
+    return AspectRatio(
+      aspectRatio: 1,
+      child: PieChart(getCharData()),
+    );
+  }
+
+  PieChartData getCharData() {
+    return PieChartData(
+      pieTouchData: PieTouchData(
+        enabled: true,
+        touchCallback: (p0, pietouchResponse) {
+          activeIndex =
+              pietouchResponse?.touchedSection?.touchedSectionIndex ?? -1;
+          setState(() {});
+        },
+      ),
+      sectionsSpace: 0,
+      sections: [
+        PieChartSectionData(
+          showTitle: false,
+          value: 40,
+          radius: activeIndex == 0 ? 35 : 25,
+          color: const Color(0xFF208BC7),
+        ),
+        PieChartSectionData(
+          value: 25,
+          radius: activeIndex == 1 ? 35 : 25,
+          showTitle: false,
+          color: const Color(0xFF4DB7F2),
+        ),
+        PieChartSectionData(
+          value: 20,
+          radius: activeIndex == 2 ? 35 : 25,
+          showTitle: false,
+          color: const Color(0xFF064060),
+        ),
+        PieChartSectionData(
+          value: 22,
+          radius: activeIndex == 3 ? 35 : 25,
+          showTitle: false,
+          color: const Color(0xFFE2DECD),
+        ),
+      ],
+    );
+  }
+}

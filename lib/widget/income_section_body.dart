8fb8301437241169b7859d8d08ed45ba2ab04316
MuhannadMutawe <muhannadrm208@gmail.com>
2026-03-19 22:21:32 +0200

    display detailed income chart  on income section body.

--- lib/widget/income_section_body.dart
+++ lib/widget/income_section_body.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:responsive_dash_board/utils/size_config.dart';
+import 'package:responsive_dash_board/widget/detailed_income_chart.dart';
 import 'package:responsive_dash_board/widget/income_char.dart';
 import 'package:responsive_dash_board/widget/income_details.dart';
 
@@ -12,7 +13,12 @@ class IncomeSectionBody extends StatelessWidget {
   Widget build(BuildContext context) {
     double width = MediaQuery.sizeOf(context).width;
     return width >= SizeConfig.desktop && width < 1750
-        ? SizedBox()
+        ? Expanded(
+            child: Padding(
+              padding: EdgeInsets.all(16),
+              child: DetailedIncomeChart(),
+            ),
+          )
         : Row(
             crossAxisAlignment: CrossAxisAlignment.center,
             children: [
